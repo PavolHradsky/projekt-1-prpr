03c202ea700acf4e2f12e2b596cb9c2af32738fb
Pavol Hradsky <hradskypalo@gmail.com>
2020-10-28 16:10:37 +0100

    uvod, head

---
+++ projekt1.c
@@ -0,0 +1,17 @@
+/*
+* FIIT STU
+* Proceduralne Programovanie
+* Autor: Pavol Hradsky
+
+* Program: Projekt 1
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(void){
+
+
+    return 0;
+}
